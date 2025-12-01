50adebb99a49d22d2651c850a1f67a2eac3701f1
arnaudromain2003-lang <arnaudromain2003@gmail.com>
2025-12-01 22:24:24 +0100

    Improve path resolution

--- src/dataframe_extraction.py
+++ src/dataframe_extraction.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import pandas as pd
 import geopandas as gpd
-FOLDER_PATH = '../'
+import os
+
+FOLDER_PATH = '..'
+base = Path(FOLDER_PATH) / "data" / "PT" / "pt_data" # Base path for the data files
 
 agg = "15min"  
 modes = ["subway", "tramway", "bus"]
-base = Path(FOLDER_PATH) / "data"
 
 dfs = {}  # mode -> DataFrame
 
@@ -18,11 +20,13 @@ for mode in ['subway','tramway','bus']:
         df.index = pd.to_datetime(df.index)
     dfs[mode] = df
     
+# Load reference data for bus and tramway stops
+
 ref_subway = pd.read_csv(f"{base}/ref_subway.csv",index_col = 0).rename(columns = {'MEAN_X' : 'lon','MEAN_Y':'lat'})
 ref_tram_bus = pd.read_csv(f"{base}/ref_tram_bus.csv",index_col = 0).rename(columns = {'IDT_PNT' : 'VAL_ARRET_CODE','COO_X_WGS84':'lon','COO_Y_WGS84':'lat'})
 
 ref_tram_bus = ref_tram_bus[['lon','lat','NOM_PNT','VAL_ARRET_CODE']]
-ref_subway = ref_subway[['lon','lat','COD_TRG','LIB_STA_SIFO']]
+# ref_subway = ref_subway[['lon','lat','COD_TRG','LIB_STA_SIFO']] a verifier
 
 df_bus = dfs["bus"].merge(ref_tram_bus, how = 'inner', on = 'VAL_ARRET_CODE')  
 df_tramway = dfs["tramway"].merge(ref_tram_bus, how = 'inner', on = 'VAL_ARRET_CODE')
@@ -32,4 +36,15 @@ df_tramway["date"]=pd.to_datetime(df_tramway["VAL_DATE"])
 
 df_bus["date_only"] = df_bus["VAL_DATE"].dt.date
 df_tramway["date_only"] = df_tramway["VAL_DATE"].dt.date
+
+# Proposition de correction car ce qui nous intersse c est le timestamp ainsi que val_date et flow:
+
+df_subway=dfs["subway"])
+df_subway['Flow']=dfs["subway"].sum(axis=1)
+
+df_bus=pd.DataFrame(df_bus, columns=['VAL_DATE', 'Flow'])
+df_tramway=pd.DataFrame(df_tramway, columns=['VAL_DATE', 'Flow'])
+df_subway=pd.DataFrame(df_subway, columns=['VAL_DATE', 'Flow'])
+
+
 
